3a1400b32908d2c0c2276b96e7ba9274006d2ce7
asif <mdasifahamed89@yahoo.com>
2022-05-15 18:12:37 +0600

    Add envirounment variable using dotenv

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
