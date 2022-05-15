01bf8a2533c526aa6fa2bb51784fe9d10a657c09
asif <mdasifahamed89@yahoo.com>
2022-05-15 23:09:41 +0600

    Level four authentication wih hashing and salting with bcrypt module

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
-const md5 = require('md5') // to encrypt user data
+const bcrypt = require('bcrypt'); // to encrypt user data
+const saltRounds = 10;
 const app = express();
 app.set("view engine", 'ejs');
 app.use(express.static("public"));
@@ -34,22 +35,30 @@ app.get("/register", function(req, res) {
 // from this post method will get user email and password throug html form
 app.post("/register", function(req, res) {
   const username = req.body.username; // recieving usename from user
-  const password = md5(req.body.password); // recieving password from user and converting it to hash using md5 module
-  const user = new User({ // storing user registration data to the database creating object its model
-    email: username,
-    password: password
-  });
-  user.save(function(err) { // saving user log in data to the data base
-    if (err) {
+  const password = req.body.password; // recieving password from user and converting it to hash using md5 module
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    if(err){
       console.log(err);
     } else {
-      res.render("secrets"); // if no error is found then only the registerd user able to see the sectrets.ejs page ejs render method
-    }
+      const user = new User({ // storing user registration data to the database creating object its model
+        email: username,
+        password: hash
+      });
+      user.save(function(err) { // saving user log in data to the data base
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets"); // if no error is found then only the registerd user able to see the sectrets.ejs page ejs render method
+        }
+
+      });
+      }
+    });
   });
-})
+
 app.post('/login', function(req, res) {
   const username = req.body.username; // from the log in page getting user name from the user
-  const password = md5(req.body.password); // from the log in page getting user password from the user and also converting it in hash with th md5 module to match with the database
+  const password = req.body.password; // from the log in page getting user password from the user and also converting it in hash with th md5 module to match with the database
   User.findOne({
     email: username
   }, function(err, foudedUser) { // after the user press log in this findone method firt look for user email that has in our datbase or not
@@ -57,12 +66,18 @@ app.post('/login', function(req, res) {
       console.log(err);
     } else {
       if (foudedUser) { // if the username i mean the email shows in the datbase then it go to next condition
-        if (foudedUser.password = password) { // if the the user email shows in the database then it check for password that has been registered with this email.
-          res.render("secrets"); // if password mathces the can be able log in as the secrets.ej page will render .
+        bcrypt.compare(password, foudedUser.password,  function(err, result) { // this bycrypt compare to compare data which has been in the datbase to return somethis 
+            if (err){
+              console.log(err);
+            }else if(result === true){
+              res.render("secrets"); // if password mathces the can be able log in as the secrets.ej page will render .
+            }
+
+        });
+
         }
       }
-    }
-  });
+    });
 });
 
 
